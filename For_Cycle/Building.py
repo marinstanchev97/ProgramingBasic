3bfdf68ee20fe1b9a74404e9b617e87f15dd90d7
marinstanchev97 <150950260+marinstanchev97@users.noreply.github.com>
2023-12-09 00:38:24 +0200

    Create Building.py

---
+++ For_Cycle/Building.py
@@ -0,0 +1,13 @@
+floors = int(input())
+rooms_per_floor = int(input())
+
+for floor in reversed(range(1, floors + 1)):
+    room_type = 'A' if floor % 2 else 'O'
+
+    if floor == floors:
+        room_type = 'L'
+
+    for room_number in range(rooms_per_floor):
+        room_name = f'{room_type}{floor}{room_number}'
+        print(room_name, end=' ')
+    print()
