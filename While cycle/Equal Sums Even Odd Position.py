7f370840dce586f961bfe0b530e41257a6227e0c
marinstanchev97 <150950260+marinstanchev97@users.noreply.github.com>
2023-12-14 05:30:52 +0200

    Create Equal Sums Even Odd Position.py

---
+++ While cycle/Equal Sums Even Odd Position.py
@@ -0,0 +1,13 @@
+first_number = int(input())
+second_number = int(input())
+for current_number in range(first_number, second_number + 1):
+    even_digits_sum = 0
+    odd_digits_sum = 0
+    current_number_as_string = str(current_number)
+    for index, digit in enumerate(current_number_as_string):
+        if index % 2 == 0:
+            odd_digits_sum += int(digit)
+        else:
+            even_digits_sum += int(digit)
+    if even_digits_sum == odd_digits_sum:
+        print(current_number, end = " ")
